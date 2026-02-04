50d4bc1a189a46369ba7fc32804ed842aad0f392
g b <ventolinester@gmail.com>
2026-02-03 23:16:18 -0500

    header accept

--- src/components/SubscribeApi.svelte
+++ src/components/SubscribeApi.svelte
@@ -13,7 +13,8 @@ async function submit() {
   try {
     const res = await fetch('/api/subscribe', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+
       body: JSON.stringify({ email, name })
     });
 
